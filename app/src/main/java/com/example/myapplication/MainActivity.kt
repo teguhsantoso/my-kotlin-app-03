9fda1747132f03438ca77db6941ee43686d4da92
Teguh Santoso <teguh.santoso@gmx.de>
2026-08-21 20:12:42 +0200

    Change the text in activity.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Kotlin for: $name!",
         modifier = modifier
     )
 }
