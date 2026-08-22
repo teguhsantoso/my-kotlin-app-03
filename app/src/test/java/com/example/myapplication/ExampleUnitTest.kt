54dee322a266f73cf3f4a98957506f551b067cc6
Teguh Santoso <teguhsantoso@users.noreply.github.com>
2026-08-22 12:33:02 +0200

    Update ExampleUnitTest.kt

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -14,9 +14,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-    @Test
-    fun add_two_and_two_should_be_four() {
-        assertEquals(4, 2 + 2)
-    }
 }
