fa955dd6a1ca4878d6e8a40b7f23b43da5d69fa1
Teguh Santoso <teguh.santoso@gmx.de>
2026-09-03 10:22:26 +0200

    Add powersync libs for this App.

---
+++ app/src/main/java/com/example/myapplication/remote/ApiClient.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.remote
+
+import com.example.myapplication.powersync.PowerSyncProvider
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiClient {
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val taskAdapter: TaskAdapter = Retrofit.Builder()
+        .baseUrl(PowerSyncProvider.BACKEND_BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(TaskAdapter::class.java)
+}
